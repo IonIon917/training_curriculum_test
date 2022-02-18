f649a0c2a07dbcecc2185666bdceb70ad4a6e593
IonIon917 <pmmadjt@gmail.com>
2022-02-18 14:42:44 +0900

    Update calendars_controller.rb

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -35,10 +35,14 @@ class CalendarsController < ApplicationController
         today_plans.push(plan.plan) if plan.date == @todays_date + x
       end
 <<<<<<< HEAD
+<<<<<<< HEAD
       days = { :month => (@todays_date + x).month, :date => (@todays_date+x).day, :plans => today_plans}
 =======
       days = { month:  (@todays_date + x).month, date: (@todays_date+ days).day, plans: today_plans}
 >>>>>>> parent of a3f1d72 (Issue3)
+=======
+      days = { month:  (@todays_date + x).month, date: (@todays_date+x).day, plans: today_plans}
+>>>>>>> parent of ee22166 (Issue2)
       @week_days.push(days)
     end
 
